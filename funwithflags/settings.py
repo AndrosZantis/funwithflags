7431b219fae791f69cfdcd12e367fd82a8656161
AndrosZantis <a.zantis1@newcastle.ac.uk>
2019-10-24 19:07:52 +0100

    bork
    woof

--- funwithflags/settings.py
+++ funwithflags/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(ezu81dr=3ik=984ueb%575hu9nx^l)+79tiyovq71%3m7#w62'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
